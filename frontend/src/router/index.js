98494db1486e78e2858ef6ffdcc58723d4de172f
TnTTNG <1260863421@qq.com>
2025-05-23 02:31:02 +0800

    第二次修改，在index.js添加了全局前置守卫

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { ElMessage } from 'element-plus'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', redirect: '/manager/home'},
-    { path: '/manager',component: () => import('@/views/Manager.vue'),
+    { path: '/', redirect: '/manager/home', meta: { requiresAuth: true}},
+    { path: '/manager',component: () => import('@/views/Manager.vue'), meta: { requiresAuth: true},
       children: [
-        { path: 'home', meta: { name: '主页' }, component: () => import('@/views/Home.vue')},
-        { path: 'admin', meta: { name: '用户管理 / 管理员信息' }, component: () => import('@/views/Admin.vue')},
-        { path: 'user', meta: { name: '主页' }, component: () => import('@/views/user/User.vue')}
+        { path: 'home', meta: { name: '主页', meta: { requiresAuth: true} }, component: () => import('@/views/Home.vue')},
+        { path: 'admin', meta: { name: '用户管理 / 管理员信息', meta: { requiresAuth: true} }, component: () => import('@/views/Admin.vue')},
+        { path: 'user', meta: { name: '主页', meta: { requiresAuth: true} }, component: () => import('@/views/user/User.vue')}
       ]
     },
-    { path: '/login', component: () => import('@/views/Login.vue') },
-    { path: '/notFound', component: () => import('@/views/404.vue') },
+    { path: '/login', component: () => import('@/views/Login.vue'), meta: { requiresAuth: false} },
+    { path: '/notFound', component: () => import('@/views/404.vue'), meta: { requiresAuth: false} },
     { path: '/:pathMatch(.*)', redirect: '/notFound' }
   ]
 })
 
+router.beforeEach((to, from, next) => {
+  const userInfo = localStorage.getItem('code_user')
+
+  if (to.meta.requiresAuth) {
+    if (!userInfo) {
+      ElMessage.warning('请先登录')
+      next({ path: '/login'})
+    } else {
+      // 已登录，允许访问
+      next()
+    }
+  } else {
+    if (to.path === '/login' && userInfo) {
+      next({ path: '/'})
+    } else {
+      next()
+    }
+
+  }
+})
 export default router
